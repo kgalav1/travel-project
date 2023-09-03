e0ff6d7821a086c198a41a83428302c9ba9751b9
kgalav1 <d.galav077@gmail.com>
2023-09-04 00:11:16 +0530

    helicopter page

--- travel/assets/js/main.js
+++ travel/assets/js/main.js
@@ -52,25 +52,25 @@ window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-const sections = document.querySelectorAll('section[id]')
+// const sections = document.querySelectorAll('section[id]')
 
-const scrollActive = () => {
-    const scrollDown = window.scrollY
+// const scrollActive = () => {
+//     const scrollDown = window.scrollY
 
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight,
-            sectionTop = current.offsetTop - 58,
-            sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+//     sections.forEach(current => {
+//         const sectionHeight = current.offsetHeight,
+//             sectionTop = current.offsetTop - 58,
+//             sectionId = current.getAttribute('id'),
+//             sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
-        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
-            sectionsClass.classList.add('active-link')
-        } else {
-            sectionsClass.classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+//         if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+//             sectionsClass.classList.add('active-link')
+//         } else {
+//             sectionsClass.classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
@@ -128,7 +128,7 @@ var swiper = new Swiper(".swiper", {
             slidesPerView: 1
         },
         1024: {
-            slidesPerView: 4
+            slidesPerView: 3
         },
         1600: {
             slidesPerView: 4
